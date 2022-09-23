e1893535b897a087746750a366141ad33d266778
MrBizzon <oleg.zubarev@mail.ru>
2022-09-23 18:53:58 +0300

    a small edit

--- Homework_Python/ДЗ от 22.09.2022/Task 1.py
+++ Homework_Python/ДЗ от 22.09.2022/Task 1.py
@@ -13,11 +13,11 @@ def day_of_week(a):
 
 
 try:
-    N = int(input("Enter a number from 1 to 7: "))
-    if N > 7 or N < 0:
+    number = int(input("Enter a number from 1 to 7: "))
+    if number > 7 or number < 0:
         print("Enter a number according to the condition")
     else:
-        if day_of_week(N):
+        if day_of_week(number):
             print("This day is a weekday")
         else:
             print("This day is a day off")
