669b95a4cf32b979f64e573b087165fedb27c9e4
MrBizzon <oleg.zubarev@mail.ru>
2022-09-24 09:21:55 +0300

    The finishing touch

--- Homework_Python/ДЗ от 22.09.2022/Task 5.py
+++ Homework_Python/ДЗ от 22.09.2022/Task 5.py
@@ -7,3 +7,18 @@
 # После сортировки
 # 1 2 3 4
 # 5 7 9 10
+
+from random import randint
+n, m = int(input("Enter the number of rows: ")), int(input("Enter the number of columns: "))
+a = [[randint(0, 100) for j in range(m)] for i in range(n)]
+
+array = []
+for i in a:
+    for i2 in i:
+        array.append(i2)
+array2 = sorted(array)
+for j in range(0, len(array2), n):
+    number = array2[j: n + j]
+    if len(number) < n:
+        number = number + [None for y in range(n - len(number))]
+    print(list(number))
