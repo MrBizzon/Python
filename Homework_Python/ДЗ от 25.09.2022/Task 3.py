816fecbb9916dd92bd356b05d3da36baedba4378
MrBizzon <info.proect@mail.ru>
2022-09-29 11:50:27 +0300

    Added a third solution

--- Homework_Python/ДЗ от 25.09.2022/Task 3.py
+++ Homework_Python/ДЗ от 25.09.2022/Task 3.py
@@ -1,2 +1,24 @@
-# Задача 3. Реализуйте алгоритм перемешивания списка. Список размерностью 10 задается случайными целыми числами, 
+# Задача 3. Реализуйте алгоритм перемешивания списка. Список размерностью 10 задается случайными целыми числами,
 # выводится на экран, затем перемешивается, опять выводится на экран. SHUFFLE нельзя юзать!
+
+import random
+first_list = []
+for num in range(10):
+    first_list.append(random.randrange(0, 30))
+print(f'The first list: ', list(first_list))
+print()
+
+
+second_list = first_list[:]
+list_length = len(second_list)
+for i in range(list_length):
+    random_index = random.randint(0, list_length - 1)
+    temp = second_list[i]
+    second_list[i] = second_list[random_index]
+    second_list[random_index] = temp
+print(f'The second list: ', second_list)
+print()
+
+result = [elem for elem in first_list if elem in second_list]
+print(f'Sorting and checking for matches: ', sorted(result))
+print()
